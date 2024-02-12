20442fa0787b6462c6a8d58c0d867d255dc81cc7
celudirosa <cluqued@live.com>
2024-02-12 17:56:53 +0100

    update producto controller

--- src/main/java/com/example/controllers/ProductoController.java
+++ src/main/java/com/example/controllers/ProductoController.java
@@ -2,6 +2,8 @@ package com.example.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,13 @@ public class ProductoController {
 
     // metodo que devuelve todos los productos en formato JSON
     @GetMapping
-    public List<Producto> findAll() {
-        return productoService.findAll();
+    public ResponseEntity<List<Producto>> findAll() {
+        var productos = productoService.findAll();
+
+        ResponseEntity<List<Producto>> responseEntity = 
+            new ResponseEntity<List<Producto>>(productos, HttpStatus.OK);
+
+        return responseEntity;
     }
 
 }
