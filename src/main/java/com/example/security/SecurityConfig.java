810ebfbd2218280b216668d23b67af659966c852
celudirosa <cluqued@live.com>
2024-02-26 15:53:00 +0100

    add stuff

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -15,8 +15,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import lombok.RequiredArgsConstructor;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 //Indicamos que se activa la seguridad web en nuestra aplicación y además esta será una clase la cual contendrá toda la configuración referente a la seguridad
@@ -43,29 +43,27 @@ public class SecurityConfig {
         return new JwtAuthenticationFilter();
     }
 
-    //Vamos a crear un bean el cual va a establecer una cadena de filtros de seguridad en nuestra aplicación.
+     //Vamos a crear un bean el cual va a establecer una cadena de filtros de seguridad en nuestra aplicación.
     // Y es aquí donde determinaremos los permisos según los roles de usuarios para acceder a nuestra aplicación
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http
-            .csrf(csrf -> csrf.disable())
-            .exceptionHandling(handling -> handling //Permitimos el manejo de excepciones
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint))
-            .sessionManagement(management -> management //Permite la gestión de sessiones
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(requests -> requests //Toda petición http debe ser autorizada
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/productos/**").hasAnyAuthority("ADMIN", "USER")
-                .requestMatchers(HttpMethod.GET, "/productos/id/**").hasAnyAuthority("ADMIN", "USER")
-                .requestMatchers(HttpMethod.DELETE, "/api/celular/eliminar/**").hasAuthority("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/api/celular/actualizar").hasAuthority("ADMIN")
-                .anyRequest().authenticated())
-            .httpBasic(withDefaults());
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(handling -> handling //Permitimos el manejo de excepciones
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(management -> management //Permite la gestión de sessiones
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests //Toda petición http debe ser autorizada
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/productos/**").hasAnyAuthority("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/productos/id/**").hasAnyAuthority("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE).hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT).hasAuthority("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
-
     }
 }
